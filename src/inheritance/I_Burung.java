87ab0ce73fcdfe4484e13d0d5d8bff4d794cc620
mellanahus <Mellanahus02SJ@gmail.com>
2022-04-28 09:51:19 +0700

    Percobaan 9 menggunakan konsep inheritance (ini merupakan bagian kelas I_Burung pada package inheritance)

---
+++ src/inheritance/I_Burung.java
@@ -0,0 +1,22 @@
+/* Percobaan menggunakan konsep inheritance
+(ini merupakan bagian kelas I_Burung
+ pada package inheritance)
+*/
+
+
+package inheritance;
+
+public class I_Burung extends J_Enemy {
+
+    void fly() {
+        System.out.println("Burung Berjalan !");
+    }
+
+    void walk() {
+        System.out.println("Burung Berjalan !");
+    }
+
+    void jump() {
+        System.out.println("Burung Loncat-Loncat");
+    }
+}
