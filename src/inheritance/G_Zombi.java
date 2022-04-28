3b9d08c93c761f3b42c62185417d56b52041ac3d
mellanahus <Mellanahus02SJ@gmail.com>
2022-04-28 09:46:51 +0700

    Percobaan 7 menggunakan konsep inheritance (ini merupakan bagian kelas G_Zombi pada package inheritance)

---
+++ src/inheritance/G_Zombi.java
@@ -0,0 +1,13 @@
+/* Percobaan menggunakan konsep inheritance
+(ini merupakan bagian kelas G_Zombi
+ pada package inheritance)
+*/
+
+package inheritance;
+
+public class G_Zombi extends J_Enemy {
+
+    void walk() {
+        System.out.println("Zombi Jalan-Jalan");
+    }
+}
